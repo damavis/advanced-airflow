fa496c0ae673b92cde89c319d1e2c5af825332a5
Cristòfol Torrens <piffall@gmail.com>
2021-04-30 10:10:00 +0200

    Error fixed on airflow-start.sh script

--- airflow-start.sh
+++ airflow-start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export AIRFLOW_IMAGE_NAME=$(basename `pwd`)/airflow
-export AIRFLOW_GID=$(id -u "${USER}")
+export AIRFLOW_UID=$(id -u "${USER}")
 export AIRFLOW_GID=0
 
 docker build -t "${AIRFLOW_IMAGE_NAME}" .
