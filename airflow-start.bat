bbc074494e6f0c6b3db20f38d495ca63b552c058
Cristòfol Torrens <piffall@gmail.com>
2021-06-03 12:04:57 +0200

    --amend

--- airflow-start.bat
+++ airflow-start.bat
@@ -2,8 +2,6 @@
 
 for %%a in ("%~dp0\.") do set "DIRNAME=%%~nxa"
 set AIRFLOW_IMAGE_NAME=%DIRNAME%/airflow
-set AIRFLOW_UID=1000
-set AIRFLOW_GID=0
 
 docker build -t %AIRFLOW_IMAGE_NAME% .
 docker-compose up -d
